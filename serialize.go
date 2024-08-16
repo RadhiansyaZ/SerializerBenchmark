08c7dbced505fb466f702076abd6b08c7b4ca1ea
alwafis <radhit78@gmail.com>
2024-08-17 00:48:03 +0700

    remove unnecessary comments and apply formatting

--- serialize.go
+++ serialize.go
@@ -2,45 +2,34 @@ package main
 
 import (
 	"encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
-	// "github.com/mitchellh/mapstructure"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
-type SortedDiscussion struct {
-	DiscussionResults []struct {
-		Orders     int    `json:"orders"`
-		Username   string `json:"username"`
-		Comment    string `json:"comment"`
-		ReplyCount int    `json:"reply_count"`
-		Replies    []struct {
-			Username string `json:"username"`
-			Comment  string `json:"comment"`
-		} `json:"replies"`
-	} `json:"discussion_results"`
-}
-
 func Serialize() []byte {
-    response, err := http.Get("http://localhost:8080/sample.json")
-
-    if err != nil {
-        fmt.Print(err.Error())
-        os.Exit(1)
-    }
-
-    responseData, err := ioutil.ReadAll(response.Body)
-    // fmt.Println(reflect.TypeOf(responseData))
+	// Variable declaration to handle the API response
 	var objmap map[string]interface{}
-	// var objmapres SortedDiscussion
+
+	// Fetching the API response
+	response, err := http.Get("http://localhost:8080/sample.json")
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	// Reading the API response
+	responseData, err := ioutil.ReadAll(response.Body)
 	if err = json.Unmarshal(responseData, &objmap); err != nil {
 		log.Fatal(err)
 	}
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Modifying the API response
 	user := objmap["discussion_results"].([]interface{})[0].(map[string]interface{})["username"].(string)
 	user = user + "_test_concat"
 	objmap["discussion_results"].([]interface{})[0].(map[string]interface{})["username"] = user
@@ -48,24 +37,26 @@ func Serialize() []byte {
 		objmap["discussion_results"].([]interface{})[i].(map[string]interface{})["orders"] = i + 1
 		objmap["discussion_results"].([]interface{})[i].(map[string]interface{})["reply_count"] = len(objmap["discussion_results"].([]interface{})[i].(map[string]interface{})["replies"].([]interface{}))
 	}
-	// mapstructure.Decode(objmap, &objmapres)
+
+	// Serializing the modified API response
 	finalres, err := json.Marshal(objmap)
 	if err != nil {
 		panic(err)
 	}
-	
-	return finalres
-	
-}
-func ActionIndex(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(Serialize())
-}
-func main() {
-	
-    http.HandleFunc("/", ActionIndex)
 
-    fmt.Println("server started at localhost:9000")
-    http.ListenAndServe(":9000", nil)
+	return finalres
+
+}
+
+func ActionIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(Serialize())
+}
+
+func main() {
+	http.HandleFunc("/", ActionIndex)
+
+	fmt.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
 	Serialize()
 }
